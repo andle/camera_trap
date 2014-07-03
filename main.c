89c475b83982197a0b22aa4385f8e446d92031ff
Andrew McGovern <mcgovern.ap@gmail.com>
2014-07-03 01:57:44 -0500

    PWM working, trigger almost
    Have PWM working on PWM module, uses TMR2. Not yet there on trigger
    sequence.

--- main.c
+++ main.c
@@ -25,33 +25,42 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <pic12f683.h>
+#include <stdint.h>
 
 #define LED GPIObits.GP0
 
 void init_ports(void)
 {
-    TRISIO = 0; // set all to outputs
+    //ANSELbits.ANS = 0b0000; // 0=digital, 1=analog, does not affect digital output
+    TRISIO = 0b000100; // IO settings: bit 2 input for PWM initialization
     GPIO = 0; // clear all IO bits
 }
 
 void init_osc(void)
 { //Internal 8MHz osc
-    OSCCONbits.IRCF = 0b110; //000 = 31 kHz, 110 = 4MHz
+    OSCCONbits.IRCF = 0b111; //0b000 = 31 kHz, 0b110 = 4MHz, 0b111 = 8MHz
 }
 
-void TMR2_init(void)
-{// initialize and start TMR2, using 31 kHz clock
+void TMR2_init(void) // does NOT increment in sleep mode
+{// initialize and start TMR2, currently for PWM
     INTCONbits.GIE = 1; // global interrupt enable
     INTCONbits.PEIE = 1; // peripheral interrupt enable
     PIE1bits.TMR2IE = 1; // Enable TMR2 interrupt
     PIR1bits.TMR2IF = 0; // clear interrupt flag
 
-    TMR2 = 0; // set TMR2 to 0
-    T2CON = 0; // reset TMR2 config
-    T2CONbits.TOUTPS = 0b1111; //prescale 16
-    T2CONbits.T2CKPS = 0b11; //postscale 16
-    PR2 = 30; // TMR2 period: 32kHz/4/16/16 = 30.27,
+    //TMR2 = 0; // set TMR2 to 0
+    //T2CON = 0; // reset TMR2 config
+    //T2CONbits.TOUTPS = 0b0; //prescale: 0b1111 = 16
+    //T2CONbits.T2CKPS = 0b0; //postscale: 0b11 = 16
+    PR2 = 51; // TMR2 period: 32kHz/4/16/16 = 30.27, ///Used in PWM for period
+    CCP1CON = 0; // reset CCP module
+    CCP1CONbits.CCP1M = 0b1100; //110x = PWM mode, active high, 111x = PWM mode active low
+    CCP1CONbits.DC1B = 00; // 2 lsb of 8 for PWM duty cycle
+    CCPR1L = 0b00011011; // CCP register low: 6msb of 8 for PWM duty cycle
+    //CCPR1H = 0b00000000; // CCP register high //
     T2CONbits.TMR2ON = 1; // TMR2 on
+    while(TMR2 !=0); // wait a cycle to start PWM
+    TRISIObits.TRISIO2 = 0; // enable PWM output
 }
 
 void TMR1_init(void) // using 31 kHz clock
@@ -80,73 +89,265 @@ void TMR1_init(void) // using 31 kHz clock
 }
 
 void TMR0_init(void)
-{// initialize and start TMR0, on/off 15 times a second if Fosc = 31 kHz
-    INTCONbits.GIE = 1; // global interrupt enable
-    INTCONbits.PEIE = 1; // peripheral interrupt enable
-    PIE1bits.TMR2IE = 1; // Enable TMR0 interrupt
+{// initialize and start TMR0,
+    //INTCONbits.GIE = 1; // global interrupt enable  ////////// done int TMR2 _init
+    //INTCONbits.PEIE = 1; // peripheral interrupt enable ////////// done int TMR2 _init
+    //PIE1bits.TMR0IE = 0; // Enable TMR0 interrupt
+    //INTCONbits.TMR0IF = 0; // clear interrupt flag
 
-    PIR1bits.TMR2IF = 0; // clear interrupt flag
-    TMR0 = 0; // set TMR2 to 0
-    OPTION_REGbits.T0CS = 0; // reset TMR2 config
+    OPTION_REG = 0; // TMR0 config
+    OPTION_REGbits.T0CS = 0; // Internal instruction cycle clock (FOSC/4)
     OPTION_REGbits.PSA = 0; // prescale assigned to TMR0
-    OPTION_REGbits.PS = 0b111; // prescaler
+    OPTION_REGbits.PS = 0b111; // prescaler: if assigned to TMR0: 0b000 = 1:2, 0b111 = 1:256,
+    /*In order to have a 1:1 prescaler value for the Timer0
+    module, the prescaler must be assigned to the WDT
+    module.*/
     T2CONbits.TMR2ON = 1; // TMR2 on
 }
 
-void modulate(long cycles)
+uint8_t sGPIO = 0;
+void trigger(void)
 {
-    int i;
-    LED = 0;
-    asm("NOP");
-    asm("NOP");
-    for(i = cycles; i != 0; i--) // 10 initial, 12 second
-    {
-        LED = 1; // 2
-        asm("NOP");
-        asm("NOP");
-        asm("NOP");
-        asm("NOP");
-        asm("NOP");
-        asm("NOP");
-        asm("NOP");
-        asm("NOP");
-        asm("NOP");
-        asm("NOP");
-        asm("NOP");
-        //asm("NOP");
-        //asm("NOP");
-        LED = 0; // 2
-        //asm("NOP");
-    }
-}
-
-bool sw; // for LED state memory
-void interrupt ISR()
-{// ISR
-    if(PIR1bits.TMR2IF)
-    {
-        TMR2 = 0; // clear TMR2
-        PIR1bits.TMR2IF = 0; // clear IF
-        sw = !sw; // switch LED
-        LED = sw; // set LED
-    }
+    sGPIO ^= (1<<0);
+    GPIO = sGPIO;
+    TMR2 = 0;
+    while(TMR0 != 16);// on 2 ms
+    sGPIO ^= (1<<0);
+    GPIO = sGPIO;
+    TMR2 = 0;
+    while(TMR0 != 217);// off 27.8 ms
+    sGPIO ^= (1<<0);
+    GPIO = sGPIO;
+    TMR2 = 0;
+    while(TMR0 != 4);// on .5
+    sGPIO ^= (1<<0);
+    GPIO = sGPIO;
+    TMR2 = 0;
+    while(TMR0 != 12);// off 1.5
+    sGPIO ^= (1<<0);
+    GPIO = sGPIO;
+    TMR2 = 0;
+    while(TMR0 != 4);// on .5
+    sGPIO ^= (1<<0);
+    GPIO = sGPIO;
+    TMR2 = 0;
+    while(TMR0 != 27);// off 3.5
+    sGPIO ^= (1<<0);
+    GPIO = sGPIO;
+    TMR2 = 0;
+    while(TMR0 != 4);// on .5
+    sGPIO ^= (1<<0);
+    GPIO = sGPIO;
+    TMR2 = 0;
+    while(TMR0 != 246);// off 63
+    TMR2 = 0;///////////////////
+    while(TMR0 != 246);/////////
+    TMR2 = 0;///////////////////
     
-    if(PIR1bits.CCP1IF)
-    {
-        // TMR1 reset by CCP mode
-        PIR1bits.CCP1IF = 0;
-        sw = !sw; // switch LED
-        LED = sw; // set LED
-    }
-
-    if(INTCONbits.T0IF)
-    {
-        // TMR0 interrupts only on rollover
-        INTCONbits.T0IF = 0; // clear IF
-        sw = !sw; // switch LED
-        LED = sw; // set LED
-    }
+    // repeat
+    sGPIO ^= (1<<0);
+    GPIO = sGPIO;
+    TMR2 = 0;
+    while(TMR0 != 16);// on 2 ms
+    sGPIO ^= (1<<0);
+    GPIO = sGPIO;
+    TMR2 = 0;
+    while(TMR0 != 217);// off 27.8 ms
+    sGPIO ^= (1<<0);
+    GPIO = sGPIO;
+    TMR2 = 0;
+    while(TMR0 != 4);// on .5
+    sGPIO ^= (1<<0);
+    GPIO = sGPIO;
+    TMR2 = 0;
+    while(TMR0 != 12);// off 1.5
+    sGPIO ^= (1<<0);
+    GPIO = sGPIO;
+    TMR2 = 0;
+    while(TMR0 != 4);// on .5
+    sGPIO ^= (1<<0);
+    GPIO = sGPIO;
+    TMR2 = 0;
+    while(TMR0 != 27);// off 3.5
+    sGPIO ^= (1<<0);
+    GPIO = sGPIO;
+    TMR2 = 0;
+    while(TMR0 != 4);// on .5
+    sGPIO ^= (1<<0);
+    GPIO = sGPIO;
+    TMR2 = 0;
+    while(TMR0 != 246);// off 63
+    TMR2 = 0;///////////////////
+    while(TMR0 != 246);/////////
+    TMR2 = 0;///////////////////
 }
+
+int times;
+void trig(void)
+{
+#asm
+    pulsecount	equ		0x08
+    times	equ		0x09
+
+        movlw	0x01
+	movwf	times
+    main
+	movlw	77
+	movwf	pulsecount
+	call 	pulsehi
+	movlw	0xff
+	movwf	pulsecount
+	call	pulselo
+	call	pulselo
+	call	pulselo
+	call	pulselo
+	movlw	48
+	movwf	pulsecount
+	call	pulselo
+	movlw	19
+	movwf	pulsecount
+	call	pulsehi
+	movlw	58
+	movwf	pulsecount
+	call	pulselo
+	movlw	19
+	movwf	pulsecount
+	call	pulsehi
+	movlw	135
+	movwf	pulsecount
+	call	pulselo
+	movlw	19
+	movwf	pulsecount
+	call	pulsehi
+	movlw	0xff
+	movwf	pulsecount
+	call	pulselo
+	call	pulselo
+	call	pulselo
+	call	pulselo
+	call	pulselo
+	call	pulselo
+	call	pulselo
+	call	pulselo
+	call	pulselo
+	movlw	125
+	movwf	pulsecount
+	call	pulselo
+	decfsz	times,1
+	goto 	main
+
+	movlw	0xff
+	movwf	pulsecount
+	call	pulselo
+
+	sleep
+
+
+pulsehi
+	bsf		GPIO,2
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop							;10
+	nop
+	nop
+	nop
+	bcf		GPIO,2
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop							;20
+	nop
+	nop
+	nop
+	decfsz	pulsecount,1
+	goto pulsehi
+	retlw	0
+
+pulselo
+	bcf		GPIO,2
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop							;10
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop							;20
+	nop
+	nop
+	nop
+	decfsz	pulsecount,1
+	goto pulselo
+	retlw	0
+
+#endasm
+}
+
+
+
+
+
+
+//bool sw; // for LED state memory
+//void interrupt ISR()
+//{// ISR
+//    if(PIR1bits.TMR2IF)
+//    {
+//        TMR2 = 0; // clear TMR2
+//        PIR1bits.TMR2IF = 0; // clear IF
+//        sw = !sw; // switch LED
+//        LED = sw; // set LED
+//    }
+//
+//    if(PIR1bits.CCP1IF)
+//    {
+//        //  ,TMR1 reset by CCP mode
+//        PIR1bits.CCP1IF = 0;
+//        sw = !sw; // switch LED
+//        LED = sw; // set LED
+//    }
+//
+//    if(INTCONbits.T0IF)
+//    {
+//        // TMR0 interrupts only on rollover
+//        INTCONbits.T0IF = 0; // clear IF
+//        sw = !sw; // switch LED
+//        LED = sw; // set LED
+//    }
+//}
+
+//void delay(void)
+//{
+//#asm
+//            clrf    TMR0
+//    w_tmr0  movf    TMR0,w
+//            xorlw   .8
+//            btfss   STATUS,z
+//            goto    w_tmr0
+//
+//    ;
+//#endasm
+//}
+
 
 int main(void)
 {
@@ -154,25 +355,18 @@ int main(void)
     init_osc();
     //TMR2_init();   /////Choose TMR init based on interrupt scheme, for now
     //TMR1_init();
-    //TMR0_init();
+    TMR0_init();
 
-//    modulate(72);
-//    delay_t_ms(278);
-//    modulate(18);
-//    delay_t_ms(15);
-//    modulate(18);
-//    delay_t_ms(35);
-//    modulate(18);
-//    delay_t_ms(630);
-//    modulate(72);
-//    delay_t_ms(278);
-//    modulate(18);
-//    delay_t_ms(15);
-//    modulate(18);
-//    delay_t_ms(35);
-//    modulate(18);
-
-    while(1);
+    int i;
+    while(1)
+    {
+        trig();
+        for(i = 20; i != 0; i--)
+        {
+            TMR0 = 0;
+            while(TMR0 != 254);
+        }
+    }
 
     return (EXIT_SUCCESS);
 }
